fdb4c54fce815541a129483ed239de7b87e5bc35
David Schmid <davethehacker@users.noreply.github.com>
2021-02-09 11:15:57 +0100

    styling

--- footer.php
+++ footer.php
@@ -1,46 +1,14 @@
         <!-- Contact -->
         <section id="contact">
-                <div class="inner">
-                    <section>
-                        
-                    </section>
-                    <section class="split">
-                        <section>
-                            <div class="contact-method">
-                                <span class="icon solid alt fa-envelope"></span>
-                                <h3>Email</h3>
-                                <a href="#">information@untitled.tld</a>
-                            </div>
-                        </section>
-                        <section>
-                            <div class="contact-method">
-                                <span class="icon solid alt fa-phone"></span>
-                                <h3>Phone</h3>
-                                <span>(000) 000-0000 x12387</span>
-                            </div>
-                        </section>
-                        <section>
-                            <div class="contact-method">
-                                <span class="icon solid alt fa-home"></span>
-                                <h3>Address</h3>
-                                <span>1234 Somewhere Road #5432<br />
-                                Nashville, TN 00000<br />
-                                United States of America</span>
-                            </div>
-                        </section>
-                    </section>
-                </div>
+                <h1>Kontakt</h1>
+                <p>Eliane Koradi</p>
+                <p>Chliacherweg 1</p>
+                <p>CH-5623</p>
+                <p>+41 56 221 26 60</p>
+                <p>info at harfenklang.ch</p>
+
             </section>
 
-        <!-- Footer -->
-            <footer id="footer">
-                <div class="inner">
-                    
-                    <ul class="copyright">
-                        <li>&copy; Untitled</li>
-                    </ul>
-                </div>
-            </footer>
 
     </div>
 
